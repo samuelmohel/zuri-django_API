2d9270460b2d338d72731ca541482fc592e5f448
samuelmohel <samuelmohel@gmail.com>
2022-07-17 23:38:51 +0100

    My first commit

--- I4G007858U9K/links/models.py
+++ I4G007858U9K/links/models.py
@@ -3,7 +3,9 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 
-
+class ActiveLinkManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(author)
     
 class Link(models.Model):
     target_url = models.URLField(max_length=200)
@@ -12,3 +14,5 @@ class Link(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
     created_date = models.DateTimeField(default=timezone.now)
     active = models.BooleanField(default=True)
+    objects = models.Manager()
+    public = ActiveLinkManager()
--- I4G007858U9K/links/urls.py
+++ I4G007858U9K/links/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = "links"
 
 urlpatterns = [
-    
+    path("active/", views.ActiveLinkView.as_view(), name='active_link'),
+    path("recent/", views.RecentLinkView.as_view(), name='recent_link'),
 ]
